e03dc98293b931db107f5400ca428e8d844505d9
jhoun <jayhoun@gmail.com>
2017-01-13 13:39:30 -1000

    grabs title with conjunctin AND

--- indexing.sql
+++ indexing.sql
@@ -1,6 +1,12 @@
 \c indexed_cars;
 
---grabs title with no duplicates
+-- grabs title with no duplicates
 SELECT DISTINCT make_title
   FROM car_models
   WHERE make_code = 'LAM';
+
+-- grabs title with conjunction AND
+SELECT DISTINCT model_title
+  FROM car_models
+  WHERE make_code = 'NISSAN'
+  AND model_code = 'GT-R';
