11302b786dc3f3304977e60a1bf1aca36ac732ef
jhoun <jayhoun@gmail.com>
2017-01-13 13:49:39 -1000

    grabs all fieldbetween 2 years

--- indexing.sql
+++ indexing.sql
@@ -11,6 +11,13 @@ SELECT DISTINCT model_title
   WHERE make_code = 'NISSAN'
   AND model_code = 'GT-R';
 
+-- grabs all the fields by make_code
 SELECT COUNT(*)
   FROM car_models
   WHERE make_code = 'LAM';
+
+-- grabs
+SELECT COUNT(*)
+  FROM car_models
+  WHERE year >= 2010
+  AND year <= 2015;
